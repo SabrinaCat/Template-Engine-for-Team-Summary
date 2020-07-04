5701e1a81b37c538eca3c8b37254ab9708292d7b
SabrinaCat <Sabrina.glow22@gmail.com>
2020-07-03 23:07:57 -0500

    classes finished and exported/ required in index.js

--- backend/classes.js
+++ backend/classes.js
@@ -4,9 +4,8 @@ class Employee {
         this.name = name;
         this.id = id;
         this.email = email;
+        let role = 'Employee';
     }
-    
-
     // Adding methods to the constructor
     getName() {
         return `${this.name}` ;
@@ -17,12 +16,10 @@ class Employee {
     getEmail() {
         return `${this.email}`
     }
-    getRole(){
-        return `Employee`
-        }
+
 }
 
-// Creating a new class from the parent
+// Creating Manager class from the parent
 class Manager extends Employee {
     constructor(officeNumber) {
         // Chain constructor with super
@@ -32,7 +29,47 @@ class Manager extends Employee {
         this.officeNumber = officeNumber;
 
         //change role to manager
-        let role = () => `Manager`;
+        let role = 'Manager';
         
     }
+}
+
+// Creating Engineer class from the parent
+class Engineer extends Employee {
+    constructor(username, url) {
+        // Chain constructor with super
+        super(name, id, email);
+
+        // Add property username and url for Github
+        this.username = username;
+        this.url = url
+
+        //change role to Engineer
+        let role = 'Engineer';
+        
+    }
+}
+
+// Creating Engineer class from the parent
+class Intern extends Employee {
+    constructor(school) {
+        // Chain constructor with super
+        super(name, id, email);
+
+        // Add property username and url for Github
+        this.school = school;
+
+        //change role to Engineer
+        let role = 'Intern';
+
+        //new method for getting school info
+        getSchool = (school) => this.school;
+        
+    }
+}
+module.exports = {
+    Employee: Employee,
+    Manager: Manager,
+    Engineer: Engineer,
+    Intern: Intern
 }
